1ad8da6e2c0701e544ae175b2deccceb50f9ddc9
gefangshuai <gefangshuai@163.com>
2017-04-06 15:13:57 +0800

    init ‘

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/index'
+import About from '@/components/about'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },{
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+})
